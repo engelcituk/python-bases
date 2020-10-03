9edcaedfd158a4cfe9ac22386ff0e567335e3485
cituk <programaciondyn3.mx@sandos.com>
2020-10-03 15:03:39 -0500

    asignacion

---
+++ conceptos/asignacion.py
@@ -0,0 +1,14 @@
+variableUno = 10
+variableDos = 'Code'
+variableTres = 10 * 20
+
+print(variableUno)
+print(variableDos)
+print(variableTres)
+#Las asignaciones anteriores se pueden reducri a una sola linea como la siguente, considera la legibilidad
+
+variableUno, variableDos , variableTres = 10, 'Code', 10*20
+
+print(variableUno)
+print(variableDos)
+print(variableTres)
