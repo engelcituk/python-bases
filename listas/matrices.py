c0fcf642b9447021e3f88e538a7f51e9de24a7b6
cituk <programaciondyn3.mx@sandos.com>
2020-10-03 17:50:41 -0500

    matrices

---
+++ listas/matrices.py
@@ -0,0 +1,8 @@
+filaUno = [10, 20]
+filaDos = [30, 40]
+
+matriz = [filaUno, filaDos]
+
+primerElemento = matriz[0][0] # se obtiene el elemento en la posicion 0,0 de la matriz
+
+print(primerElemento)
