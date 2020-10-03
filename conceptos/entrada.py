6796330a1749bf595385ee2dc849670b466c3332
cituk <programaciondyn3.mx@sandos.com>
2020-10-03 16:41:36 -0500

    Entrada de datos por teclado

---
+++ conceptos/entrada.py
@@ -0,0 +1,19 @@
+print('¿Cual es tú nombre?')
+nombre = input() #con input se puede leer lo que se ingresa por consola, y regresa un string
+
+print('¿Cual es tú edad?')
+edad = int( input() ) #convertimos el string en entero
+
+print('¿Cual es tú peso?')
+peso = float( input() ) #convertimos el string a numero flotante, se puede reducir las lineas poniendo la pregunta como argumento en input
+
+print('¿Estas autorizado (si/no)?')
+autorizado = input() == 'si' 
+
+print('hola', nombre, '\n')
+print('edad', edad)
+print('peso', peso)
+print('autorizado', autorizado)
+
+
+
