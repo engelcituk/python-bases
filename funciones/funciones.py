9cf536e91656a4c5b294b33254f580d29f666265
cituk <programaciondyn3.mx@sandos.com>
2020-10-04 12:18:03 -0500

    Múltiples valores de entrada y salida

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -2,3 +2,19 @@ def saluda():
     print('hola mundo')
 
 saluda()
+
+def crearMensaje(nombre):
+    return 'Hola {}, Bienvenido al curso de Python 3'.format(nombre)
+
+print(crearMensaje('Juanito'))
+
+def suma(valor1, valor2, valor3, valor4):
+    return valor1 + valor2 + valor3 + valor4
+
+print(suma(1,2,3,4))
+
+def getCourse():
+    return 'Curso de Python', 'super', 3.8
+
+curso, nivel, version  = getCourse()
+print(curso, nivel, version)
