97d318bc1400507ddb605d5e69cf41d4a4c42298
cituk <programaciondyn3.mx@sandos.com>
2020-10-04 00:30:06 -0500

    Ciclo for

---
+++ ciclos-condicionales/for.py
@@ -0,0 +1,17 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+for numero in numeros:
+    print(numero)
+
+print("\n")
+
+diccionario  = {"a":1, "b":2}
+for llave in diccionario:
+    print(llave)
+
+
+print("\n")
+
+valores = ( (10, 20, 30), ["strings", "strings", " "], (True, False, True) )
+
+for valor1, valor2, valor3 in valores:
+  print(valor1, valor2, valor3)
