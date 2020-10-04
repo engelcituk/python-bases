42be120f10c6113103dce3f1f155ad6c1607bebb
cituk <programaciondyn3.mx@sandos.com>
2020-10-04 00:13:49 -0500

    Condicionales

---
+++ ciclos-condicionales/condicionales.py
@@ -0,0 +1,17 @@
+colorLuz = 'amarillo'
+
+if colorLuz == 'verde':
+    print('Puede continuar')
+elif colorLuz == 'amarillo':
+    print('Mucha precaucion')
+else: #el uso de else es optativo
+    print('No Puede continuar')
+
+
+variable = True
+if variable:
+  print('Es valor es verdadero')
+else:
+  print('El valor es falso')
+
+#valores que python toma como False: False, "", '', [], (), {}, 0 y 0.0
