17f9eadcaa46d140a9040f24d21f8f205d5015e4
cituk <programaciondyn3.mx@sandos.com>
2020-10-03 19:39:57 -0500

    de listas a tuplas, y viceversa

---
+++ tuplas/listasTuplas.py
@@ -0,0 +1,16 @@
+lista = ["hola", "mundo", "como", "estan"]
+tupla = ("introduccion", "basico", "listas", "tuplas")
+string = "python"
+
+nuevaTupla = list(tupla)
+nuevaLista = tuple(lista)
+tuplaString = list(string)
+listaString = tuple(string)
+
+
+print(nuevaTupla)
+print(nuevaLista)
+print(tuplaString)
+print(listaString)
+
+
