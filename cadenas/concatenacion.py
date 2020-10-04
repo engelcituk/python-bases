ddf2150c4f6427b2915ff1e3497729f188c8a32f
cituk <programaciondyn3.mx@sandos.com>
2020-10-03 21:28:10 -0500

    concatenacion

---
+++ cadenas/concatenacion.py
@@ -0,0 +1,5 @@
+texto = "Curso de python "
+
+curso = "c" + texto[1:] + str(3) + " super cool"#la conccatenacion se logra con el signo mas, y no se puede concatenar un valor que no sea string
+
+print(curso)
