407cda2d814c26636f42615428f41d97283131fc
cituk <programaciondyn3.mx@sandos.com>
2020-10-04 16:35:27 -0500

    Formas de imports

--- modulos-paquetes/principal.py
+++ modulos-paquetes/principal.py
@@ -1,3 +1,23 @@
-import calculadora
+# import calculadora
+#from calculadora import * #hace referencia a que se va utilizar todas la funciones del modulo calculadora
+from calculadora import suma, resta, multiplicacion, division #se puede o omitir el asterisco indicando especificamente el método a ocupar
 
-print (calculadora.suma(20, 30))
+""" podemos hacer saltos de linea a las importaciones si estas son demasiadas y ponerlas enntre parentesis
+from calculadora import (suma,
+                         resta,
+                         multiplicacion,
+                         division) """
+
+"""
+    tambien se pueden poner alias a las importaciones:
+
+    from calculadora import suma as adicion
+
+    print (adicion(20, 30)) 
+
+"""
+
+
+#print (calculadora.suma(20, 30)) #otra forma de hacer un import
+print (suma(20, 30)) #otra forma de hacer un import
+print (division(30, 2)) #si se llama una funcion sin importarla en el modulo, habrá un error
