2f2060a8ef6224932290d3371d855208c662cf31
cituk <programaciondyn3.mx@sandos.com>
2020-10-04 00:54:29 -0500

    Asignación de valores mediante if

---
+++ ciclos-condicionales/asignacion.py
@@ -0,0 +1,18 @@
+#Asignación de valores mediante if
+cal = 10
+color = None
+
+if cal >= 7:
+    color = 'verde'
+else:
+    color=  'rojo'
+
+print(cal, color)
+
+
+#reduciendo lineas de codigo
+calificacion = 10 #input
+color = 'verde' if calificacion >= 7 else 'rojo'
+
+print(calificacion, color)
+
