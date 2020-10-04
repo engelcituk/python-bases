1b6efb5b246b5ebbdd5cdbc15204176665a38851
cituk <programaciondyn3.mx@sandos.com>
2020-10-03 23:39:54 -0500

    llaves, items y valores

---
+++ diccionarios/llaves.py
@@ -0,0 +1,16 @@
+diccionario  = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8}
+
+keys = diccionario.keys() #otenemos todas las llaves
+valores = diccionario.values() #otenemos todos los valores
+items = diccionario.items() #otenemos todos los items (llave y valor)
+tupla = tuple(keys) #se puede transformar las llaves en tupla
+lista = list(keys) #se puede transformar las llaves en lista
+listaValores = list(valores) #se puede transformar los valores en una lista
+
+
+print(tupla)
+print(lista)
+print(listaValores)
+print(items)
+
+
